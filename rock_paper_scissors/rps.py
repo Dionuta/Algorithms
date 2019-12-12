02acca9ebf70ae6f7e1fdbdb5eb50815233064b1
Dionuta <diounta@gmail.com>
2019-12-12 00:57:21 -0800

    testing

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,12 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  if n <= 0:
+    return [[]]
+  if n == 1:
+    return  [['rock'], ['paper'], ['scissors']]
+  
+    
 
 
 if __name__ == "__main__":
